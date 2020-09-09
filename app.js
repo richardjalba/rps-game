b8a4d28471ed08f2276cfb11e01ef06233f3f284
Richard Alba <64925670+richardjalba@users.noreply.github.com>
2020-09-09 14:54:09 -0700

    Add files via upload
    Added dummy pictures and some functionality.

---
+++ app.js
@@ -0,0 +1,85 @@
+// Cache the DOM
+let userScore = 0;
+let computerScore = 0;
+
+// DOM variables within span tag
+// _ signifies it's a DOM variable
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_p = document.querySelector(".result > p");
+
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+// Caching means to store something for future use
+// We stored all these things in variables for future use
+
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function convertToWord() {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  if (letter === "s") return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
+}
+
+function lose() {
+  console.log("LOSE");
+}
+
+function draw() {
+  console.log("DRAW");
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+// userChoice isn't traditionally defined
+// It is contextually defined after game() below
+
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+
+main();
